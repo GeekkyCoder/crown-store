aed699fed185b9fee88a8abecd3f05243e619b92
Faraz <farazatgithub@gmail.com>
2023-01-31 10:30:12 +0500

    implmented useReducer hook in userAuthContext

--- src/Context/UserAuthContext.jsx
+++ src/Context/UserAuthContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useEffect, useState } from "react";
+import React, { createContext, useEffect, useReducer } from "react";
 
 import {
   onAuthUserStateChange,
@@ -7,8 +7,36 @@ import {
 
 const Context = createContext();
 
+const USER_ACTION_TYPES = {
+   SET_CURRENT_USER:"SET_CURRENT_USER"
+}
+
+const initialState = {
+  currentUser: null
+}
+
+const userReducer = (state, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case "SET_CURRENT_USER":
+      return {
+        ...state,
+        currentUser: payload,
+      };
+    default:
+      return state;
+  }
+};
+
 const UserAuthProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  // const [currentUser, setCurrentUser] = useState(null);
+
+  const [{currentUser},dispatch] = useReducer(userReducer,initialState)
+
+  const setCurrentUser = (user) => {
+    dispatch({type:USER_ACTION_TYPES.SET_CURRENT_USER,payload:user})
+    console.log("dispatched")
+  }
 
   useEffect(() => {
     const unsub = onAuthUserStateChange(async (user) => {
