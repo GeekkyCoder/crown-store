30e80b4e9d65c71d633ba22fbe381ceddb094ba9
Faraz <farazatgithub@gmail.com>
2023-02-03 10:53:35 +0500

    tweaks and improvments in catogories selector logic

--- src/store/catogories/catogories_selector.js
+++ src/store/catogories/catogories_selector.js
@@ -1,1 +1,6 @@
-export const catogoriesSelector = state => state.catogories.catogories
+export const catogoriesSelector = (state) =>
+  state.catogories.catogories.reduce((acc, querySnapShot) => {
+    const { items, title } = querySnapShot;
+    acc[title.toLowerCase()] = items;
+    return acc;
+  }, {});
