4f345764a41ef4e350de328ab47cc5168e793f62
Faraz <farazatgithub@gmail.com>
2023-02-07 11:37:21 +0500

    initial setup for stripe payment

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,21 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { CatogoriesProvider } from "./Context/ProductContext";
 import { Provider } from "react-redux";
 import { persistor, store } from "./store/store";
-import { PersistGate } from "redux-persist/integration/react"; 
+import { PersistGate } from "redux-persist/integration/react";
+import { Elements } from "@stripe/react-stripe-js";
+
+import { stripePromise } from "./utils/firebase/stripe/stripe";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <Provider store={store}>
         <PersistGate persistor={persistor}>
-        <CatogoriesProvider>
-          <App />
-        </CatogoriesProvider>
+          <CatogoriesProvider>
+            <Elements stripe={stripePromise}>
+              <App />
+            </Elements>
+          </CatogoriesProvider>
         </PersistGate>
       </Provider>
     </Router>
